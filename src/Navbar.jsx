9b37c38141537c6ca79e8c00c77e75a40b5f6bc9
shivu1919 <shivamthakur.ss129@gmail.com>
2025-05-29 23:39:52 +0530

    second commit

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -15,20 +15,12 @@ const Navbar = () => {
         <ul>
           <li><Link className="link-tag" to="/" onClick={() => setMenuVisible(false)}>Home</Link></li>
           <li><Link className="link-tag" to="/about" onClick={() => setMenuVisible(false)}>About</Link></li>
-          <li><Link className="link-tag" to="/services" onClick={() => setMenuVisible(false)}>My Services</Link></li>
+          <li><Link className="link-tag" to="/services" onClick={() => setMenuVisible(false)}>Services</Link></li>
           <li><Link className="link-tag" to="/contact" onClick={() => setMenuVisible(false)}>Contact</Link></li>
         </ul>
       </div>
 
-      <div id="nav-menu">
-        <img
-          id="menu"
-          src="/menu.png"
-          onClick={showMenu}
-          alt="menu icon"
-          style={{ width: '30px', cursor: 'pointer' }}
-        />
-      </div>
+
     </div>
   )
 }
