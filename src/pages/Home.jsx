e2c78c87cc6e514332dfcfa042990ee9ee826954
shivu1919 <shivamthakur.ss129@gmail.com>
2025-05-30 00:42:29 +0530

    fourth commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,8 +7,17 @@ function Home() {
         <div className='main-home'>
             <div id="main-home-first">
                 <div id="my-pic">
-                    <img src="/mypic.png" style={{height:"260px",width:"260px", borderRadius:"35px 0 35px 0"}}/>
+                    <img src="/mypic.png" style={{height:"260px",width:"260px", borderRadius:"40px 0 40px 0"}}/>
+
+                <div id="my-name">
+                        <p style={{color:"#ffffff", backgroundColor:"transparent", fontSize:"40px", fontWeight:"700", textAlign:"center"}}>Thakur Shivam Kumar</p>
+                        <p style={{color:"#ffffffaa", backgroundColor:"transparent", fontSize:"20px"}}>Senior Software Engineer</p>
+                        <p style={{color:"#ffffffaa", backgroundColor:"transparent", fontSize:"20px"}}>Corporate & Technical Trainer</p>
+                        
                 </div>
+                </div>
+
+
             </div>
         </div>
     </>
